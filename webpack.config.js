d8660cbed1c0fb42d9436467d81739baae4a414a
Rory Devane <rory@heytempo.com>
2019-06-06 16:41:50 +0100

    Fixed build script and ran first build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
     entry: path.resolve(__dirname, 'src/lib/index.js'),
     output: {
-        path: path.resolve(__dirname, './build/lib'),
+        path: path.resolve(__dirname, 'dist/lib'),
         filename: 'index.js',
         library: '',
         libraryTarget: 'commonjs'
